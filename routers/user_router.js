6f35e77fd55eec2bb2ddd9bb922a014984c1d5e5
KROHNICK <business.kanekperez@gmail.com>
2019-02-13 15:32:22 -0800

    UserRouter get endpoints

--- routers/user_router.js
+++ routers/user_router.js
@@ -2,4 +2,32 @@ const express = require("express");
 const db = require("../data/helpers/userDb");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    let users = await db.get();
+    res.status(200).json(users);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ err: "The users information could not be retrieved." });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await db.getById(req.params.id);
+    if (post.length > 0) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist." });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ err: "The user information could not be retrieved." });
+  }
+});
+
 module.exports = router;
