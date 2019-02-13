6239bfb5b16db051cfee7f496fca02c6f37da8db
KROHNICK <business.kanekperez@gmail.com>
2019-02-13 15:34:19 -0800

    UserRouter delete id endpoint

--- routers/user_router.js
+++ routers/user_router.js
@@ -49,4 +49,19 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const count = await db.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "The user has been nuked" });
+    } else {
+      res.status(404).json({ message: "The user could not be found" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error removing the user"
+    });
+  }
+});
+
 module.exports = router;
